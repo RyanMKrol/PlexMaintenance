e85025325fa3021486058d79f3b01d88443b3109
RyanMKrol <ryankrol.m@gmail.com>
2023-07-15 22:59:10 +0100

    Add main to fetch plex data, and pair up with tmdb data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-process.stdout.write('Hello World!');
+/* eslint-disable */
+import { fetchPlexLibraryDetails, fetchTvItemId, fetchTvItemDetails } from './fetch';
+import { haltPromise } from './utils';
+
+/**
+ * Main
+ */
+async function main() {
+  const plexLibraryDetails = await fetchPlexLibraryDetails();
+
+  const data = await plexLibraryDetails.slice(0,2).reduce(async (acc, currentLibItem) => acc.then(async (currentAcc) => {
+    const id = await fetchTvItemId(currentLibItem.title, currentLibItem.year);
+    const deets = await fetchTvItemDetails(id);
+
+    currentAcc.push({
+        plex: currentLibItem,
+        reference: deets
+    })
+
+    // wait some time to prevent us overloading the API
+    await haltPromise(5000);
+
+    return currentAcc
+  }), Promise.resolve([]));
+}
+
+main();
